b47c0df349bbcd96a82fdac8e968434541c8094e
cainus <gregg@caines.ca>
2012-04-29 09:52:19 -0700

    made nodetest.js match the doc example

--- nodetest.js
+++ nodetest.js
@@ -1,11 +1,12 @@
 var http = require('http')
+var http = require('http')
 var detour = require('./detour').detour;
 var d = new detour();
-d.route('', {GET : function(req, res){res.end("test");}})
+var exampleResource = {GET : function(req, res){res.end("test");}}
+d.route('', exampleResource)
 var server = http.createServer(function(req, res){
-                                d.dispatch(req, res)
-             });
-server.listen(9999, function(){ 
-  console.log("listening on 9999");
-});
-
+                                 d.dispatch(req, res)
+                               });
+server.listen(9999, function(){
+                       console.log("listening on 9999");
+                    });
